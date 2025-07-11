74271377474505c15e47343450ddade3f8efb066
Vinicius Yudi <vinizzly2005@gmail.com>
2025-07-10 21:39:44 -0300

    add -> rooms listing

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { CreateRoom, } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <div>
-      <Button>Hello World</Button>
-
-    </div>
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route element={<Room />} path="/room/:roomId" />
+      </Routes>
+    </BrowserRouter>
+    </QueryClientProvider>
   )
-  
 }
